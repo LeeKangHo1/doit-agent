054d0fa2b94deb32c563692b3e1dd44414679c81
kangho.lee.pr <kangho.lee.pr@gmail.com>
2025-07-04 15:09:09 +0900

    8장 rag기반 스트림릿 챗봇 완성

---
+++ chap09/sec03/rag.py
@@ -0,0 +1,74 @@
+# sec03/rag.py
+
+import streamlit as st
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+
+# import retriever -> retriever.py 안에 retriever객체가 있어서 헷갈릴 수 있음. retriever.retriever로 호출은 가능함
+from retriever import query_augmentation_chain, retriever, document_chain
+
+# 모델 초기화
+llm = ChatOpenAI(model="gpt-4o-mini")
+
+# 사용자의 메시지를 처리하는 함수
+def get_ai_response(messages, docs):
+    response = document_chain.stream({
+        "messages": messages,
+        "context": docs,
+    })
+
+    for chunk in response:
+        yield chunk
+
+# Streamlit 앱
+st.title("💬 GPT-4o-mini LangChain Chat")
+
+# 스트림릿 session_state에 메시지 저장
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [
+        SystemMessage("너는 문서에 기반해 답변하는 도시 정책 전문가야.")
+    ]
+
+# 스트림릿 화면에 메시지 출력
+for msg in st.session_state.messages:
+    if msg.content:
+        if isinstance(msg, SystemMessage):
+            st.chat_message("system").write(msg.content)
+        elif isinstance(msg, AIMessage):
+            st.chat_message("assistant").write(msg.content)
+        elif isinstance(msg, HumanMessage):
+            st.chat_message("user").write(msg.content)
+
+# 사용자 입력 처리
+if prompt := st.chat_input():
+    st.chat_message("user").write(prompt) # 사용자 메시지 출력
+    st.session_state.messages.append(HumanMessage(prompt)) # 사용자 메시지 저장
+    print("user\t:", prompt) # , 적을 경우 자동으로 공백 1칸 설정
+
+    augmented_query = query_augmentation_chain.invoke({
+        "messages": st.session_state["messages"],
+        "query": prompt
+    })
+    print("augmented_query\t", augmented_query)
+
+    # 관련 문서 검색
+    print("관련 문서 검색")
+    # gpt는 augmented_query만 넣는 것을 추천
+    docs = retriever.invoke(f"{prompt}\n{augmented_query}")
+
+    for doc in docs:
+        print('----------')
+        print(doc)
+
+        # 문서 출처 표기하기
+        with st.expander(f"**문서:** {doc.metadata.get('source', '알 수 없음')}"): # source가 없으면 '알 수 없음'이 기본값
+            # 파일명과 페이지 정보 표시
+            st.write(f"**page:**{doc.metadata.get('page', '')}") # page를 알 수 없으면 '' 이 기본값
+            st.write(doc.page_content)
+
+    print("===============")
+
+    with st.spinner(f"AI가 답변을 준비 중입니다... '{augmented_query}'"):
+        response = get_ai_response(st.session_state["messages"], docs)
+        result = st.chat_message("assistant").write_stream(response)
+    st.session_state["messages"].append(AIMessage(result))
---
+++ chap09/sec03/retriever.py
@@ -0,0 +1,55 @@
+# sec03/retriever.py
+
+# 임베딩 모델 선언하기
+from langchain_openai import OpenAIEmbeddings
+embedding = OpenAIEmbeddings(model='text-embedding-3-large')
+
+# 언어 모델 불러오기
+from langchain_openai import ChatOpenAI
+llm = ChatOpenAI(model="gpt-4o-mini")
+
+# Load Chroma store
+from langchain_chroma import Chroma
+print("Loading existing Chroma store")
+
+# 벡터 db 절대 경로  
+persist_directory = 'C:\workspace\doit-agent\chap09\chroma_store'
+
+vectorstore = Chroma(
+    persist_directory=persist_directory,
+    embedding_function=embedding,
+)
+
+# Create retriever 
+retriever = vectorstore.as_retriever(k=3)
+
+# Create document chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.output_parsers import StrOutputParser
+
+question_answering_prompt = ChatPromptTemplate(
+    [
+        (
+            "system",
+            "사용자의 질문에 대해 아래 context에 기반하여 답변하라.:\n\n{context}",
+        ),
+        MessagesPlaceholder(variable_name="messages"),
+    ]
+)
+
+document_chain = create_stuff_documents_chain(llm, question_answering_prompt) | StrOutputParser()
+
+# query augmentation chain
+query_augmentation_prompt = ChatPromptTemplate.from_messages(
+    [
+        MessagesPlaceholder(variable_name="messages"),
+        (
+            "system",
+            "기존의 대화 내용을 활용하여 사용자가 질문한 의도를 파악해서 한 문장의 명료한 질문으로 변환하라. 대명사나 '이', '저, '그' 와 같은 표현을 명확한 명사로 표현하라. :\n\n{query}"
+        ),
+    ]
+)
+
+# chain 순서 주의. 입력값 -> 메시지 -> 응답 -> 문자열
+query_augmentation_chain = query_augmentation_prompt | llm | StrOutputParser()
